49e3555cc182f69d0e936f402c436889077240a0
Jesper Andersson <jsprA86@gmail.com>
2015-05-31 23:44:03 +0200

    Finished assignment 1

--- app/src/main/java/se/mah/idk/pfi3_2015_jesper_a/MainActivity.java
+++ app/src/main/java/se/mah/idk/pfi3_2015_jesper_a/MainActivity.java
@@ -2,16 +2,81 @@ package se.mah.idk.pfi3_2015_jesper_a;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String[] quotes;
+    private static final String TAG = "myLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quotes = getResources().getStringArray(R.array.quote);
+
+        Log.i(TAG, "onCreate");
+    }
+
+    public void displayQuote(View view){
+        String quote = getRandomQuote();
+        //set text to quote?
+        TextView v = (TextView) findViewById(R.id.textQuote);
+        v.setText(quote);
+        Log.i(TAG, "displayQuote");
+    }
+
+    public String getRandomQuote(){
+        Random rand = new Random();
+        int q = rand.nextInt(quotes.length);
+        return quotes[q];
+    }
+
+
+    //LIFECYCLE
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
     }
 
     @Override
@@ -35,4 +100,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
